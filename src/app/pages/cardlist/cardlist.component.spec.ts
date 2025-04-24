12f4881fc490928801507a79ca680ad3121a9214
VioletteCrochet <violettecrc@gmail.com>
2025-04-24 14:36:31 +0200

    test: test

--- src/app/pages/cardlist/cardlist.component.spec.ts
+++ src/app/pages/cardlist/cardlist.component.spec.ts
@@ -16,8 +16,4 @@ describe('CardlistComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/pages/register/register.component.spec.ts
+++ src/app/pages/register/register.component.spec.ts
@@ -17,7 +17,4 @@ describe('RegisterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
